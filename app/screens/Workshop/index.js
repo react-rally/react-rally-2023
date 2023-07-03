f0d3d05c6cc404cf913123b0daba7cf6f2997a88
Matt Zabriskie <mzabriskie@apple.com>
2023-07-03 09:24:02 -0600

    Updating workshop

--- app/screens/Workshop/index.js
+++ app/screens/Workshop/index.js
@@ -8,7 +8,7 @@ export default () => {
   return (
     <div className="Workshop">
       <section className="highlight">
-        {constants.Meta.EVENT_NAME} features two incredible workshop days preceding the conference.
+        {constants.Meta.EVENT_NAME} features two incredible workshops preceding the conference.
         Come sharpen your skills with expert instruction from some of the most knowledgable people in the React community.
         Whether you are a newcomer to React wanting to get your feet wet, or have been using React for years our workshops
         have something to offer you!
@@ -19,11 +19,11 @@ export default () => {
           <h2>Intro to React</h2>
 
           <p>
-            A <strong>two day</strong> workshop held <strong>August 15-16, 2023</strong> being taught by <strong>Michael Chan</strong>.
+            A <strong>one day</strong> workshop held <strong>August 16, 2023</strong> being taught by <strong>Michael Chan</strong>.
           </p>
 
           <p>
-            React can be overwhelming, with countless concepts and a rich history to navigate. That's why we've crafted this beginner-friendly, two-day workshop to help you carve a path through the React ecosystem. Our seasoned instructor, chantastic, will be your guide, offering expert insight and support. Together, you'll explore JSX WTF’s, styling approaches, composition patterns, TypeScript fundamentals, powerful code organization, editor superpowers, and so much more. By the end of the workshop, you'll possess the skills and confidence to build React apps independently or collaboratively.
+            React can be overwhelming, with countless concepts and a rich history to navigate. That's why we've crafted this beginner-friendly, workshop to help you carve a path through the React ecosystem. Our seasoned instructor, chantastic, will be your guide, offering expert insight and support. Together, you'll explore JSX WTF’s, styling approaches, composition patterns, TypeScript fundamentals, powerful code organization, editor superpowers, and so much more. By the end of the workshop, you'll possess the skills and confidence to build React apps independently or collaboratively.
           </p>
 
           <p>
@@ -39,14 +39,27 @@ export default () => {
         </section>
 
         <section className="Workshop__Sessions__Item">
-          <h2>Web App Fundamentals - Remix</h2>
+          <h2>Full Stack Foundations</h2>
 
           <p>
             A <strong>one day</strong> workshop held <strong>August 16, 2023</strong> being taught by <strong>Kent C. Dodds</strong>.
           </p>
 
           <p>
-            Building modern web applications is riddled with complexity. And that's only if you bother to deal with the problems. Tired of wiring up onSubmit to backend APIs and making sure your client-side cache stays up-to-date? Wouldn't it be cool to be able to use the global nature of CSS to your benefit, rather than find tools or conventions to avoid or work around it? And how would you like nested layouts with intelligent and performance optimized data management that just works™? Remix solves some of these problems, and completely eliminates the rest. You don't even have to think about server cache management or global CSS namespace clashes. It's not that Remix has APIs to avoid these problems, they simply don't exist when you're using Remix. Oh, and you don't need that huge complex graphql client when you're using Remix. They've got you covered. Ready to build faster apps faster?
+            You're busy shipping software,<br/><br/>
+            You've got tons of responsibility.<br/><br/>
+            Solving problems with the simplest, most optimal pattern is absolutely one of them.<br/><br/>
+            Shipping quickly is another.<br/><br/>
+            In your rush to get things out the door, you can often skip over many important foundational concepts that would simplify your life a great deal without even knowing it. Even if you've been doing this for a long time, keeping up with advancements in software development is challenging (I promise, you don't have to use <code>display: table</code> to vertically center divs anymore 😩).<br/><br/>
+            This workshop will fill gaps in your knowledge you didn't even know you had and update your mental models for the NOW!<br/><br/>
+            In the time we go through this together, we'll cover modern best practices across the stack:<br/>
+            <ul>
+              <li>Styling</li>
+              <li>Routing</li>
+              <li>Loading & Mutating Data</li>
+              <li>Client-side Scripting</li>
+            </ul>
+            We'll cover a lot of ground, giving you a solid foundation for everything else you do on the web. ⚡️
           </p>
 
           <Person
