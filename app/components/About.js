8e1857da98feb91ec87987e01fd36ed90ff43fb0
Mike North <mikel.north@gmail.com>
2023-07-18 09:47:50 -0600

    Update About.js

--- app/components/About.js
+++ app/components/About.js
@@ -7,13 +7,12 @@ export default () => {
   return (
     <section className="highlight">
       <p>
-        {constants.Meta.CONFERENCE_NAME} is a two day, single track conference
-        for developers of all backgrounds using React.js, React Native, and
-        related tools. Speakers will cover topics like React Native, state
-        management, hooks, universal JavaScript, and more. Whether you're a
-        seasoned developer who has been using React since before it was cool,
-        or a newcomer to the scene, {constants.Meta.CONFERENCE_NAME} has
-        something for everyone!
+        {constants.Meta.CONFERENCE_NAME} is a two day conference designed for developers of all backgrounds, 
+        from seasoned professionals to those just starting out. With a single track program, 
+        you'll be immersed in a variety of exciting presentations that cover a range of topics, 
+        including state management, hooks, accessibility, performance, JavaScript, native applications, 
+        career growth, and more. Our inclusive conference is a great place to network, learn, and share experiences with like-minded individuals.
+        Don't miss this chance to take your skills to the next level and join us at {constants.Meta.CONFERENCE_NAME}.
       </p>
     </section>
   );
